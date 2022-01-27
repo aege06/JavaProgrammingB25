35335eeef76382fb00ffbdbffe2410269635927d
aege06 <aysegulkucuk06006@gmail.com>
2022-01-27 00:24:05 -0600

    updated

---
+++ src/myPractices/Book.java
@@ -0,0 +1,21 @@
+package myPractices;
+
+public class Book {
+    int pages;
+    static String author;
+    static{
+        author="Stephen King";
+    }
+    public void set(int pages){
+        this.pages=pages;
+    }
+
+    public static void main(String[] args) {
+        Book b1=new Book();
+        b1.set(500);
+        b1.author="Christopher P";
+        Book b2=new Book();
+        System.out.println(b1.author.equals(b2.author)+ " ");
+        System.out.println(b1.pages==b2.pages);
+    }
+}
